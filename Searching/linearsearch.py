ec39f8836077fe8eb198a8458d95074b63339d73
KaranRaval <117911284+KaranRaval123@users.noreply.github.com>
2023-09-30 15:54:48 +0530

    Update linearsearch.py

--- Searching/linearsearch.py
+++ Searching/linearsearch.py
@@ -1,13 +1,18 @@
-def linearSearch(arr, N, target):
-  
-    for i in range(0, N):
-        if (arr[i] == target):
-            return i
+# def linearSearch(arr, N, target):
+#     for i in range(0, N):
+#         if (arr[i] == target):
+#             return i
+#     return -1
+def linearSearch(arr, N, target,counter):
+    if counter < N:
+        if (arr[counter] == target):
+            return counter
+        return linearSearch(arr, N, target, counter+1)
     return -1
 arr = [2, 3, 4, 10, 40]
 target = 10
 N = len(arr)
-result = linearSearch(arr, N, target)
+result = linearSearch(arr, N, target,0)
 if(result == -1):
     print("Element is not present in array")
 else:
