fbdd7412a8e724f73632d1a48042956b63738585
KaranRaval <117911284+KaranRaval123@users.noreply.github.com>
2023-10-09 19:22:24 +0530

    Update insertionSort.ts

--- Sorting/insertionSort.ts
+++ Sorting/insertionSort.ts
@@ -1,11 +1,14 @@
-const selectionSort = (arr) => {
+const insertionSort = (arr) => {
+  let j;
   for (let i = 1; i < arr.length; i++) {
-    while (i> 0 && arr[i-1] > arr[i]) {
-      [arr[i], arr[i-1]] = [arr[i-1],arr[i]]
-      i--
+    j = i - 1;
+    while (j >= 0 && arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      j--;
     }
   }
   return arr;
 };
-const a = [12,12,1,1,1,1,39,27,24,26,0,26,35,24,46];
-console.log(selectionSort(a));
+
+const a = [12, 12, 1, 1, 1, 1, 39, 27, 24, 26, 0, 26, 35, 24, 46];
+console.log(insertionSort(a));
